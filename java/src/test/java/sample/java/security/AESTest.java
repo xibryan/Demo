5ea117a32ca3bad2b094f2ad01f7276c0075ecc0
Yuan <xborant@hotmail.com>
2016-04-20 01:22:56 +0800

    AES

---
+++ java/src/test/java/sample/java/security/AESTest.java
@@ -0,0 +1,17 @@
+package sample.java.security;
+
+import org.junit.Test;
+
+public class AESTest {
+    @Test
+    public void test() {
+        String value = "test";
+        String passwd = "Test12345_";
+
+        byte[] encode = AES.encrypt(value, passwd);
+        System.out.println(encode);
+
+        String decode = AES.decrypt(encode, passwd);
+        System.out.println(decode);
+    }
+}

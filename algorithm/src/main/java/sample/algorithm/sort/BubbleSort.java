5999805d0fd8b25714ef9f140eed5a9a62f37d42
Yuan <xborant@hotmail.com>
2016-05-27 01:10:57 +0800

    BubbleSort

--- algorithm/src/main/java/sample/algorithm/sort/BubbleSort.java
+++ algorithm/src/main/java/sample/algorithm/sort/BubbleSort.java
@@ -5,17 +5,22 @@ public class BubbleSort
 {
     public static void sort(int[] data)
     {
-        for (int i = data.length - 1; i > 0; i--)
+        for (int i = data.length; i > 1; i--)
         {
-            for (int j = 0; j < i; j++)
+            for (int j = 1; j < i; j++)
             {
-                if (data[j] > data[j + 1])
+                if (data[j - 1] > data[j])
                 {
-                    int tmp = data[j];
-                    data[j] = data[j + 1];
-                    data[j + 1] = tmp;
+                    swap(data, j, j + 1);
                 }
             }
         }
+    }
+
+    private static void swap(int[] data, int i, int j)
+    {
+        int tmp = data[i - 1];
+        data[i - 1] = data[j - 1];
+        data[j - 1] = tmp;
     }
 }

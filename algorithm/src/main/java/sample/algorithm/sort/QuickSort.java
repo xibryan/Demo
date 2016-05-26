4a7cca217c6692261fc7f289afbd08e0b38e2356
Yuan <xborant@hotmail.com>
2016-05-27 01:00:17 +0800

    QuickSort

---
+++ algorithm/src/main/java/sample/algorithm/sort/QuickSort.java
@@ -0,0 +1,76 @@
+package sample.algorithm.sort;
+
+public class QuickSort
+{
+    public static void sort(int[] data)
+    {
+        quickSort(data, 1, data.length);
+    }
+
+    private static void quickSort(int[] data, int start, int end)
+    {
+        if (start < end)
+        {
+            selectBaseNumber(data, start, end);
+            int pos = partition(data, start, end);
+            quickSort(data, start, pos - 1);
+            quickSort(data, pos + 1, end);
+        }
+    }
+
+    private static void selectBaseNumber(int[] data, int start, int end)
+    {
+        int pos = 0;
+        int middle = (start + end) / 2;
+        int startNum = data[start - 1], endNum = data[end - 1], middleNum = data[middle - 1];
+        if (startNum > endNum)
+        {
+            pos = (endNum > middleNum) ? end : ((startNum > middleNum) ? middle : start);
+        }
+        else
+        {
+            // i <= j
+            pos = (endNum < middleNum) ? end : ((startNum < middleNum) ? middle : start);
+        }
+
+        if (pos != end)
+        {
+            swap(data, pos, end);
+        }
+    }
+
+    private static int partition(int[] data, int start, int end)
+    {
+        int base = data[end - 1];
+        int i = start, j = end - 1;
+        while (i <= j)
+        {
+            if (data[i - 1] <= base)
+            {
+                ++i;
+                continue;
+            }
+
+            if (data[j - 1] >= base)
+            {
+                --j;
+                continue;
+            }
+
+            if (i < j)
+            {
+                swap(data, i, j);
+            }
+        }
+
+        swap(data, i, end);
+        return i;
+    }
+
+    private static void swap(int[] data, int i, int j)
+    {
+        int tmp = data[i - 1];
+        data[i - 1] = data[j - 1];
+        data[j - 1] = tmp;
+    }
+}

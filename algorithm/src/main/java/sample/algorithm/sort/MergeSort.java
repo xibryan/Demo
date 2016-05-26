a6150e34287e3ec3f70e5bad51eb99a116b07661
Yuan <xborant@hotmail.com>
2016-05-26 23:35:55 +0800

    MergeSort

---
+++ algorithm/src/main/java/sample/algorithm/sort/MergeSort.java
@@ -0,0 +1,57 @@
+package sample.algorithm.sort;
+
+public class MergeSort
+{
+    public static void sort(int[] data)
+    {
+        sort(data, 1, data.length);
+    }
+
+    private static void sort(int[] data, int start, int end)
+    {
+        if (start >= end)
+        {
+            return;
+        }
+
+        int middle = (start + end) / 2;
+        sort(data, start, middle);
+        sort(data, middle + 1, end);
+        merge(data, start, middle, end);
+    }
+
+    private static void merge(int[] data, int start, int middle, int end)
+    {
+        int[] tmp = new int[end - start + 1];
+        int idxTmp = 0;
+        int i = start, j = middle + 1;
+        while (i <= middle || j <= end)
+        {
+            if (i > middle)
+            {
+                tmp[idxTmp++] = data[j++ - 1];
+                continue;
+            }
+
+            if (j > end)
+            {
+                tmp[idxTmp++] = data[i++ - 1];
+                continue;
+            }
+
+            if (data[i - 1] < data[j - 1])
+            {
+                tmp[idxTmp++] = data[i++ - 1];
+            }
+            else
+            {
+                tmp[idxTmp++] = data[j++ - 1];
+            }
+        }
+
+        for (int j2 = 0; j2 < tmp.length; j2++)
+        {
+            data[start + j2 - 1] = tmp[j2];
+        }
+    }
+}
